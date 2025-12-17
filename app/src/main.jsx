c039257e3a2aaa8a4560f7e22d25dc5aeaa5f7ac
RyanH <paccloud@gmail.com>
2025-12-17 12:31:41 -0500

    feat: add vercel analytics

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 )
