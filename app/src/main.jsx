6c07e2c55922dfb29bed3a7ccbf75b7a7402b56a
RyanH <paccloud@gmail.com>
2026-01-17 13:11:09 -0500

    Move Vercel analytics to root

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App />
-      <Analytics />
     </ThemeProvider>
+    <Analytics />
   </StrictMode>,
 )
